e07ea70996f39727aa7904703177fb7afc4896b3
ismetkync <ismetkync737@gmail.com>
2022-08-30 17:58:32 +0300

    finishing-project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
-
 import Explore from "./pages/Explore";
-import Profile from "./pages/Profile";
-import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
 import Category from "./pages/Category";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
+import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
 	return (
@@ -29,9 +30,16 @@ function App() {
 					<Route path='/sign-up' element={<SignUp />} />
 					<Route path='/forgot-password' element={<ForgotPassword />} />
 					<Route path='/create-listing' element={<CreateListing />} />
+					<Route path='/edit-listing/:listingId' element={<EditListing />} />
+					<Route
+						path='/category/:categoryName/:listingId'
+						element={<Listing />}
+					/>
+					<Route path='/contact/:landlordId' element={<Contact />} />
 				</Routes>
 				<Navbar />
 			</Router>
+
 			<ToastContainer />
 		</>
 	);
